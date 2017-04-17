3d1bcf908c722515507d0d6bd0e8631297769ebf
Simon <si.ashbery@gmail.com>
2017-04-17 15:55:31 +0000

    Completed challenge 1:3

--- session1/challenge/3_simple_logic.rb
+++ session1/challenge/3_simple_logic.rb
@@ -8,5 +8,13 @@
 # ten_twenty(6) # => 10
 
 def ten_twenty(n)
-  # your code goes here
+    if
+     n.even? == true
+        return 10
+    else
+        return 20
+    end
 end
+
+ten_twenty(5)
+ten_twenty(6)
