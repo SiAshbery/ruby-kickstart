b28786ab1ffa0d2f743687bcfbcb4bbd86b6ed31
Simon <si.ashbery@gmail.com>
2017-04-20 15:50:01 +0000

    Challenge 2:8

--- session2/challenge/8_array.rb
+++ session2/challenge/8_array.rb
@@ -5,3 +5,8 @@
 # got_three? ['a', 'a', 'b']  # => false
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
+
+def got_three?(array)
+   array.each_cons(3){|x,y,z| return true if x == y && y==z}
+   false
+end
