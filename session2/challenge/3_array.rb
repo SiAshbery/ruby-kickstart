766fba9213e6405ebb956469078a965b59c75bad
Simon <si.ashbery@gmail.com>
2017-04-21 10:27:21 +0000

    Challenge 2:3

--- session2/challenge/3_array.rb
+++ session2/challenge/3_array.rb
@@ -5,7 +5,22 @@
 # "abcdefg".every_other_char  # => "aceg"
 # "".every_other_char         # => ""
 
+#class String
+ # def every_other_char
+  #  letters = chars
+   # to_return = letters.select{|x|letters.index(x).even?}.join("")
+    #to_return
+  #end
+#end
+
 class String
   def every_other_char
+    letters = chars
+    every_other = {}
+    letters.each_with_index{|item, index| every_other[index] = item}
+    letters = []
+    every_other.delete_if{|key, value| key.odd?}.each{|key, value| letters << value}
+    letters.join
   end
 end
+
